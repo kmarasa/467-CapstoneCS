a5cd72500f84f895cd9a1b033f636876fa20f977
Margaret Barnes <barnmarg@oregonstate.edu>
2026-07-19 14:25:37 -0600

    clang take 3

--- include/DHT20.h
+++ include/DHT20.h
@@ -6,14 +6,15 @@ Define DHT20 object to store and be used
 to run the DHT20 sensor.
 */
 
-#include "hardware/i2c.h"
-#include "pico/stdlib.h"
 #include <inttypes.h>
 #include <math.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#include "hardware/i2c.h"
+#include "pico/stdlib.h"
 
 /*
 Defines DHT20 object that will store information
@@ -24,9 +25,9 @@ crc (uint8_t) stores the checksum to verify the data from the sensor
 bytes (uint8_t) stores the last retrieved set of bytes
 */
 typedef struct DHT20 {
-  float humidity;
-  uint32_t lastRead;
-  uint8_t bytes[7];
+    float humidity;
+    uint32_t lastRead;
+    uint8_t bytes[7];
 } DHT20;
 
 /*
@@ -35,17 +36,17 @@ for its first time. Will create I2C controller
 and initialize the values of the sensor.
 Returns 0 if successful
 */
-int start_DHT20_sensor(DHT20 *sensor);
+int start_DHT20_sensor(DHT20* sensor);
 
 /*
 Public function that requests, retrieves and
 processes measurement from the DHT20 sensor.
 Return 0 if successful
 */
-int take_measurement(DHT20 *sensor);
+int take_measurement(DHT20* sensor);
 
 /*
 Public function to retrieve the humidity value store.
 Returns last retrieved humidity value as float
 */
-float get_humidity(DHT20 *sensor);
+float get_humidity(DHT20* sensor);
