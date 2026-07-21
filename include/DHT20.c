f1450336e550f32fe9573e283871c78b4b239334
doritnelson <106699826+doritnelson@users.noreply.github.com>
2026-07-21 14:10:19 -0400

    Fixed checksum
    Rewrote completely. Discarded using the lookup table and rewrote the function to just calculate the checksum using the polynomial and initial values in the documentation. Also removed the bypass previously used in code and moved verify checksum to the take_measurement function (as the other data verifications are in that function)

--- include/DHT20.c
+++ include/DHT20.c
@@ -37,29 +37,37 @@ static const int cry_inside = 100;
 static const int attempts = 5;
 
 /*
-Lookup table for quicker checking of CRC
-Take from: https://oshgarage.com/the-crc8-checksum/
-CRC 8 lookup table
+Private function used to verify the bytes of
+data sent by the sensor using CRC8 maxim
+with initial value of 0xFF according to pg. 10
+of DHT20 documentation.
+Returns true(1) if match
 */
-static const uint8_t CRC_8_TABLE[256] = {
-    0,   94,  188, 226, 97,  63,  221, 131, 194, 156, 126, 32,  163, 253, 31,
-    65,  157, 195, 33,  127, 252, 162, 64,  30,  95,  1,   227, 189, 62,  96,
-    130, 220, 35,  125, 159, 193, 66,  28,  254, 160, 225, 191, 93,  3,   128,
-    222, 60,  98,  190, 224, 2,   92,  223, 129, 99,  61,  124, 34,  192, 158,
-    29,  67,  161, 255, 70,  24,  250, 164, 39,  121, 155, 197, 132, 218, 56,
-    102, 229, 187, 89,  7,   219, 133, 103, 57,  186, 228, 6,   88,  25,  71,
-    165, 251, 120, 38,  196, 154, 101, 59,  217, 135, 4,   90,  184, 230, 167,
-    249, 27,  69,  198, 152, 122, 36,  248, 166, 68,  26,  153, 199, 37,  123,
-    58,  100, 134, 216, 91,  5,   231, 185, 140, 210, 48,  110, 237, 179, 81,
-    15,  78,  16,  242, 172, 47,  113, 147, 205, 17,  79,  173, 243, 112, 46,
-    204, 146, 211, 141, 111, 49,  178, 236, 14,  80,  175, 241, 19,  77,  206,
-    144, 114, 44,  109, 51,  209, 143, 12,  82,  176, 238, 50,  108, 142, 208,
-    83,  13,  239, 177, 240, 174, 76,  18,  145, 207, 45,  115, 202, 148, 118,
-    40,  171, 245, 23,  73,  8,   86,  180, 234, 105, 55,  213, 139, 87,  9,
-    235, 181, 54,  104, 138, 212, 149, 203, 41,  119, 244, 170, 72,  22,  233,
-    183, 85,  11,  136, 214, 52,  106, 43,  117, 151, 201, 74,  20,  246, 168,
-    116, 42,  200, 150, 21,  75,  169, 247, 182, 232, 10,  84,  215, 137, 107,
-    53};
+static int verify_checksum(DHT20 *sensor) {
+  // sets initial value of crc
+  uint8_t crc = 0xFF;
+  // iterates through the bytes of data
+  for (int i = 0; i < 6; i++) {
+    // xors crc with the current byte
+    crc = crc ^ sensor->bytes[i];
+    // iterates through the digits in the byte
+    for (int j = 0; j < 8; j++) {
+      /*
+      Checks if the current digit is 1 then
+      shifts to the left and applies the polynomial
+      function (as according to pg. 10) if the
+      current digit is 1
+      */
+      if (crc & 0x80) {
+        crc = (crc << 1) ^ 0x31;
+      } else {
+        crc = (crc << 1);
+      }
+    }
+  }
+  // last byte is the crc sent by the sensor
+  return crc == sensor->bytes[6];
+}
 
 /*
 Defined on pg. 10 of DHT20 Documentation
@@ -193,23 +201,10 @@ static int retrieve_measure(DHT20 *sensor) {
 }
 
 /*
-Private function used to verify the bytes of
-data sent by the sensor using CRC8 maxim
-with initial value of 0xFF according to pg. 10
-of DHT20 documentation. Use table lookup for
-faster calculation.
-Returns zero if match
+Private function that retrieves the data relevant to
+the humidity value then converts it according to the
+formula provided by pg. 11 in the DHT20 documentation.
 */
-static int verify_checksum(DHT20 *sensor) {
-  int i;
-  uint8_t CRC = 0xFF;
-
-  for (i = 0; i < 6; i++)
-    CRC = CRC_8_TABLE[CRC ^ sensor->bytes[i]];
-
-  return sensor->bytes[6] == CRC;
-}
-
 static int convert_humidity(DHT20 *sensor) {
   // according to pg. 10, humidity measurement
   // is store in the 1st, 2nd and 1st half of the 3rd byte
@@ -219,11 +214,6 @@ static int convert_humidity(DHT20 *sensor) {
   raw <<= 4;
   raw += (sensor->bytes[3] >> 4);
   sensor->humidity = raw / pow(2, 20) * 100;
-
-  // verify checksum
-  if (!verify_checksum(sensor)) {
-    return incorrect_checksum;
-  }
 
   return 0;
 }
@@ -256,9 +246,12 @@ int take_measurement(DHT20 *sensor) {
     sleep_ms(10);
   }
 
-  // the 0 return is to bypass a known bug
-  convert_humidity(sensor);
-  return 0;
+  // verify checksum
+  if (!verify_checksum(sensor)) {
+    return incorrect_checksum;
+  }
+
+  return convert_humidity(sensor);
 }
 
 /*
